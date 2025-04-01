fad663e851d0769acf528bb1e35f7b8cc2c92363
brendankhow <brendankhow.2022@scis.smu.edu.sg>
2025-04-01 10:29:16 +0800

    wq

--- js/loadData.js
+++ js/loadData.js
@@ -2,11 +2,7 @@ let cachedData = null;
 
 export async function loadRawData() {
   if (!cachedData) {
-    // Use a CORS proxy to fetch the file
-    const corsProxy = "https://proxy.cors.sh/";
-
-    const googleDriveUrl = "https://drive.google.com/uc?export=download&id=1Q7yPGguzVwps9a9foUprtQZ7j_bqV8K6";
-    cachedData = await d3.csv(corsProxy + googleDriveUrl);
+    cachedData = await d3.csv("data/updated_crime_data_2015_2024.csv");
     
     cachedData.forEach(d => {
       d.Year = +d.Year;
