98a1a2e59df0f23d809df4e28418bff2b25ed7ae
brendankhow <brendankhow.2022@scis.smu.edu.sg>
2025-04-01 10:04:46 +0800

    dw

--- js/loadData.js
+++ js/loadData.js
@@ -2,9 +2,7 @@ let cachedData = null;
 
 export async function loadRawData() {
   if (!cachedData) {
-    // Use the proxy endpoint to fetch the CSV file
-    const proxyUrl = "/api/proxy";
-    cachedData = await d3.csv(proxyUrl);
+    cachedData = await d3.csv("data/updated_crime_data_2015_2024.csv");
     cachedData.forEach(d => {
       d.Year = +d.Year;
       d.District = +d.District;
