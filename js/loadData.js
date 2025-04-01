4e23144d9e713200d335087322bc1a72f5da02bc
brendankhow <brendankhow.2022@scis.smu.edu.sg>
2025-04-01 10:28:39 +0800

    wq

--- js/loadData.js
+++ js/loadData.js
@@ -3,7 +3,8 @@ let cachedData = null;
 export async function loadRawData() {
   if (!cachedData) {
     // Use a CORS proxy to fetch the file
-    const corsProxy = "https://cors-anywhere.herokuapp.com/";
+    const corsProxy = "https://proxy.cors.sh/";
+
     const googleDriveUrl = "https://drive.google.com/uc?export=download&id=1Q7yPGguzVwps9a9foUprtQZ7j_bqV8K6";
     cachedData = await d3.csv(corsProxy + googleDriveUrl);
     
