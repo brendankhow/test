413707f8e24728bbd8b1f417d7e76ab1ad11b33a
brendankhow <brendankhow.2022@scis.smu.edu.sg>
2025-04-01 09:49:05 +0800

    e

--- js/loadData.js
+++ js/loadData.js
@@ -2,7 +2,9 @@ let cachedData = null;
 
 export async function loadRawData() {
   if (!cachedData) {
-    cachedData = await d3.csv("data/updated_crime_data_2015_2024.csv");
+    // Use the public URL of the CSV file hosted on Vercel Blob
+    const blobUrl = "https://bakkny68bbpifx3q.public.blob.vercel-storage.com/updated_crime_data_2015_2024-N3Np07WgA1t6B8O3X59TahV4aH2Jvd.csv";
+    cachedData = await d3.csv(blobUrl);
     cachedData.forEach(d => {
       d.Year = +d.Year;
       d.District = +d.District;
