57a87f920e7f45fa48ff04ba046bb89a69c3b029
brendankhow <brendankhow.2022@scis.smu.edu.sg>
2025-04-01 10:15:46 +0800

    e2

--- js/loadData.js
+++ js/loadData.js
@@ -3,6 +3,7 @@ let cachedData = null;
 export async function loadRawData() {
   if (!cachedData) {
     cachedData = await d3.csv("data/updated_crime_data_2015_2024.csv");
+    
     cachedData.forEach(d => {
       d.Year = +d.Year;
       d.District = +d.District;
