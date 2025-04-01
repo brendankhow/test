409ab937957acb2e4a25821b1657f1bb6d484999
brendankhow <brendankhow.2022@scis.smu.edu.sg>
2025-04-01 10:27:27 +0800

    wq

--- js/loadData.js
+++ js/loadData.js
@@ -1,10 +1,11 @@
-// filepath: /Users/brendankhow/Documents/GitHub/test/js/loadData.js
 let cachedData = null;
 
 export async function loadRawData() {
   if (!cachedData) {
-    // Use the direct download link from Google Drive
-    cachedData = await d3.csv("https://drive.google.com/uc?export=download&id=1Q7yPGguzVwps9a9foUprtQZ7j_bqV8K6");
+    // Use a CORS proxy to fetch the file
+    const corsProxy = "https://cors-anywhere.herokuapp.com/";
+    const googleDriveUrl = "https://drive.google.com/uc?export=download&id=1Q7yPGguzVwps9a9foUprtQZ7j_bqV8K6";
+    cachedData = await d3.csv(corsProxy + googleDriveUrl);
     
     cachedData.forEach(d => {
       d.Year = +d.Year;
