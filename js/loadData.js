69a0708d9c2f09a9afcae7bc33e1852bb6433d01
brendankhow <brendankhow.2022@scis.smu.edu.sg>
2025-04-01 10:26:17 +0800

    w

--- js/loadData.js
+++ js/loadData.js
@@ -1,8 +1,10 @@
+// filepath: /Users/brendankhow/Documents/GitHub/test/js/loadData.js
 let cachedData = null;
 
 export async function loadRawData() {
   if (!cachedData) {
-    cachedData = await d3.csv("data/updated_crime_data_2015_2024.csv");
+    // Use the direct download link from Google Drive
+    cachedData = await d3.csv("https://drive.google.com/uc?export=download&id=1Q7yPGguzVwps9a9foUprtQZ7j_bqV8K6");
     
     cachedData.forEach(d => {
       d.Year = +d.Year;
